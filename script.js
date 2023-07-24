46dba1f1e7c9357e8641593ca6a20b34b18c5b4d
Carolina S Fernandes <carolsfernandes@outlook.com>
2023-07-24 07:15:39 -0300

    melhorias no menu

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+// temas
 function toggleTheme() {
   var body = document.querySelector('body');
   body.classList.toggle('dark');
 }
+
+// rolagem do menu
+window.addEventListener('scroll', function() {
+  let header = document.querySelector('.header');
+  header.classList.toggle('scrolled', window.scrollY > 100);
+});
